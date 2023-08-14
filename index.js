e7108a4257a0366bd92a93edd74895bdfa1ea82b
Ramkumar1713 <141205031+Ramkumar1713@users.noreply.github.com>
2023-08-14 14:53:32 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const adfsConfig = require('./adfs');
-const {envConfig} = require('../envConfig');
-
-module.exports = {
-  adfsConfig,
-  envName: envConfig.ENV || 'dev',
-  authStrategy: envConfig.AUTH_STRATEGY || 'adfs',
-};
